b6eabd22181f825f743bca1ddecf706dc38c7527
yuqingKitty <582650269@qq.com>
2019-08-21 23:17:48 +0800

    Initial commit

---
+++ app/src/main/java/com/aotuo/h3officeplat/activity/SplashActivity.java
@@ -0,0 +1,15 @@
+package com.aotuo.h3officeplat.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.aotuo.h3officeplat.R;
+
+public class SplashActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+
+}

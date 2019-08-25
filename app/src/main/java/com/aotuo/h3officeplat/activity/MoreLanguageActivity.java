703714aabea00a247cec67031ab55a6b4444a5f5
yuqingKitty <582650269@qq.com>
2019-08-25 18:21:21 +0800

    1.0多语言UI页面

--- app/src/main/java/com/aotuo/h3officeplat/activity/MoreLanguageActivity.java
+++ app/src/main/java/com/aotuo/h3officeplat/activity/MoreLanguageActivity.java
@@ -1,8 +1,31 @@
 package com.aotuo.h3officeplat.activity;
 
-import com.aotuo.h3officeplat.R;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class MoreLanguageActivity extends BaseActivity {
+import com.aotuo.h3officeplat.R;
+import com.aotuo.h3officeplat.view.TitleView;
+
+import butterknife.BindView;
+
+public class MoreLanguageActivity extends BaseActivity implements View.OnClickListener {
+    @BindView(R.id.title_view)
+    TitleView title_view;
+    @BindView(R.id.rl_language_chinese)
+    RelativeLayout rl_language_chinese;
+    @BindView(R.id.tv_language_chinese)
+    TextView tv_language_chinese;
+    @BindView(R.id.iv_chinese_selected)
+    ImageView iv_chinese_selected;
+    @BindView(R.id.rl_language_english)
+    RelativeLayout rl_language_english;
+    @BindView(R.id.tv_language_english)
+    TextView tv_language_english;
+    @BindView(R.id.iv_english_selected)
+    ImageView iv_english_selected;
+
 
     @Override
     protected int getLayout() {
@@ -11,6 +34,27 @@ public class MoreLanguageActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-
+        title_view.getBackBtn().setOnClickListener(this);
+        title_view.getRightTitle().setOnClickListener(this);
+        rl_language_chinese.setOnClickListener(this);
+        rl_language_english.setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.tv_right_title:
+                // 完成
+                break;
+            case R.id.rl_language_chinese:
+
+                break;
+            case R.id.rl_language_english:
+                break;
+        }
+    }
+
 }

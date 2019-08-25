a24b837a4ec82328554b0e68afdaccc2e4c95e09
yuqingKitty <582650269@qq.com>
2019-08-25 18:08:49 +0800

    1.0UI页面

--- app/src/main/java/com/aotuo/h3officeplat/activity/EditServerAddressActivity.java
+++ app/src/main/java/com/aotuo/h3officeplat/activity/EditServerAddressActivity.java
@@ -1,8 +1,23 @@
 package com.aotuo.h3officeplat.activity;
 
-import com.aotuo.h3officeplat.R;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
 
-public class EditServerAddressActivity extends BaseActivity {
+import com.aotuo.h3officeplat.R;
+import com.aotuo.h3officeplat.view.TitleView;
+
+import butterknife.BindView;
+
+public class EditServerAddressActivity extends BaseActivity implements View.OnClickListener {
+    @BindView(R.id.title_view)
+    TitleView title_view;
+    @BindView(R.id.iv_delete_server)
+    ImageView iv_delete_server;
+    @BindView(R.id.et_serve_address)
+    EditText et_serve_address;
+
+
     @Override
     protected int getLayout() {
         return R.layout.activity_edit_server_address;
@@ -10,6 +25,23 @@ public class EditServerAddressActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        title_view.getBackBtn().setOnClickListener(this);
+        title_view.getRightTitle().setOnClickListener(this);
+        iv_delete_server.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_back:
+                finish();
+                break;
+            case R.id.tv_right_title:
+                // 保存
+                break;
+            case R.id.iv_delete_server:
+                et_serve_address.setText("");
+                break;
+        }
     }
 }
--- app/src/main/java/com/aotuo/h3officeplat/activity/ServerAddressActivity.java
+++ app/src/main/java/com/aotuo/h3officeplat/activity/ServerAddressActivity.java
@@ -1,8 +1,22 @@
 package com.aotuo.h3officeplat.activity;
 
-import com.aotuo.h3officeplat.R;
+import android.view.View;
+import android.widget.TextView;
 
-public class ServerAddressActivity extends BaseActivity {
+import com.aotuo.h3officeplat.R;
+import com.aotuo.h3officeplat.view.TitleView;
+
+import butterknife.BindView;
+
+public class ServerAddressActivity extends BaseActivity implements View.OnClickListener {
+    @BindView(R.id.title_view)
+    TitleView title_view;
+    @BindView(R.id.tv_saved_serve_address)
+    TextView tv_saved_serve_address;
+    @BindView(R.id.tv_check_next)
+    TextView tv_check_next;
+
+
     @Override
     protected int getLayout() {
         return R.layout.activity_server_address;
@@ -10,6 +24,25 @@ public class ServerAddressActivity extends BaseActivity {
 
     @Override
     protected void initView() {
+        title_view.getBackBtn().setOnClickListener(this);
+        title_view.getRightTitle().setOnClickListener(this);
+        tv_check_next.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.iv_back:
+
+                break;
+            case R.id.tv_right_title:
+                // 编辑
+                changeView(EditServerAddressActivity.class);
+                break;
+            case R.id.tv_check_next:
+                // 检测
+
+                break;
+        }
     }
 }

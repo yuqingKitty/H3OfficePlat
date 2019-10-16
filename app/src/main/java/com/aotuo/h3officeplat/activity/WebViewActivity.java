aae65ee0cfca2180cd4524a05a57f4238e70a77a
yuqingKitty <582650269@qq.com>
2019-10-17 00:22:49 +0800

    1.0webview input file

--- app/src/main/java/com/aotuo/h3officeplat/activity/WebViewActivity.java
+++ app/src/main/java/com/aotuo/h3officeplat/activity/WebViewActivity.java
@@ -147,38 +147,40 @@ public class WebViewActivity extends BaseActivity {
             }
 
             /**
-             * 8(Android 2.2) <= API <= 10(Android 2.3)回调此方法
+             * // For Android < 3.0
              */
-            private void openFileChooser(android.webkit.ValueCallback<Uri> uploadMsg) {
+            private void openFileChooser(ValueCallback<Uri> uploadMsg) {
                 // (2)该方法回调时说明版本API < 21，此时将结果赋值给 mUploadCallbackBelow，使之 != null
                 mUploadCallbackBelow = uploadMsg;
-                takePhoto();
+                showFileChooser();
             }
 
             /**
-             * 11(Android 3.0) <= API <= 15(Android 4.0.3)回调此方法
+             * // For Android >= 3.0
              */
-            public void openFileChooser(android.webkit.ValueCallback<Uri> uploadMsg, String acceptType) {
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
                 // 这里我们就不区分input的参数了，直接用拍照
-                openFileChooser(uploadMsg);
+                mUploadCallbackBelow = uploadMsg;
+                showFileChooser();
             }
 
             /**
-             * 16(Android 4.1.2) <= API <= 20(Android 4.4W.2)回调此方法
+             * //For Android  >= 4.1
              */
-            public void openFileChooser(android.webkit.ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
                 // 这里我们就不区分input的参数了，直接用拍照
-                openFileChooser(uploadMsg);
+                mUploadCallbackBelow = uploadMsg;
+                showFileChooser();
             }
 
             /**
-             * API >= 21(Android 5.0.1)回调此方法
+             * //For Android  >= 5.0
              */
             @Override
             public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> valueCallback, FileChooserParams fileChooserParams) {
                 // (1)该方法回调时说明版本API >= 21，此时将结果赋值给 mUploadCallbackAboveL，使之 != null
                 mUploadCallbackAboveL = valueCallback;
-                takePhoto();
+                showFileChooser();
                 return true;
             }
         });
@@ -187,32 +189,22 @@ public class WebViewActivity extends BaseActivity {
     /**
      * 调用相机
      */
-    private void takePhoto() {
+    private void showFileChooser() {
+        Intent fileIntent = new Intent(Intent.ACTION_GET_CONTENT);
+        fileIntent.addCategory(Intent.CATEGORY_OPENABLE);
+        fileIntent.setType("*/*");
+
+        Intent captureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // 指定拍照存储位置的方式调起相机
         String filePath = Environment.getExternalStorageDirectory() + File.separator + Environment.DIRECTORY_PICTURES + File.separator;
         String fileName = "IMG_" + DateFormat.format("yyyyMMdd_hhmmss", Calendar.getInstance(Locale.CHINA)) + ".jpg";
         imageUri = Uri.fromFile(new File(filePath + fileName));
-
-//        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-//        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
-//        startActivityForResult(intent, REQUEST_CODE);
-
-
-        // 选择图片（不包括相机拍照）,则不用成功后发刷新图库的广播
-//        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
-//        i.addCategory(Intent.CATEGORY_OPENABLE);
-//        i.setType("image/*");
-//        startActivityForResult(Intent.createChooser(i, "Image Chooser"), REQUEST_CODE);
-
-        Intent captureIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
         captureIntent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
 
-        Intent Photo = new Intent(Intent.ACTION_PICK,
-                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
 
-        Intent chooserIntent = Intent.createChooser(Photo, "Image Chooser");
-        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{captureIntent});
-
+        Intent Photo = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+        Intent chooserIntent = Intent.createChooser(Photo, "");
+        chooserIntent.putExtra(Intent.EXTRA_INITIAL_INTENTS, new Parcelable[]{captureIntent,fileIntent});
         startActivityForResult(chooserIntent, REQUEST_CODE);
     }
 
